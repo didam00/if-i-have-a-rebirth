92b46b962b5e289c9d243365bfab31ad8d10c8d5
didam00 <didam00@naver.com>
2025-07-18 02:17:11 +0900

    fix: 빌드 시 경로 이상 오류 해결

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'url';
 
 export default defineConfig({
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
