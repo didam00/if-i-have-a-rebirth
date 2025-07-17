dfdafba20e66833866256b3d48c096db122e4587
didam00 <didam00@naver.com>
2025-07-18 02:36:26 +0900

    fix: ㄹㅇ 최종

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'url';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/if-i-have-a-rebirth/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
